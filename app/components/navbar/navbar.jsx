d878cc2a7db4769caf0c0b3d0c386f89790a141f
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-11-20 11:15:05 +0100

    user logout function is operational

--- app/components/navbar/navbar.jsx
+++ app/components/navbar/navbar.jsx
@@ -1,9 +1,9 @@
 "use client";
-import { UserAuth } from "@/app/context/AuthContext";
+import { UserAuth } from "../../context/AuthContext";
 import React from "react";
 
 const Navbar = () => {
-  const { logOut } = UserAuth;
+  const { user, logOut } = UserAuth();
   const handleSignOut = async () => {
     try {
       await logOut();
@@ -32,30 +32,38 @@ const Navbar = () => {
     },
   ];
   return (
-    <div className="fixed w-screen h-20 flex items-center justify-between border-b-2 border-solid border-blue-500 bg-white">
-      <div className="w-48 h-fit ml-10">
-        <img src="/logo.png" alt="Logo" className="" />
-      </div>
-      <div className="flex items-center justify-center mr-10">
-        <div className="ml-10 flex items justify-between">
-          {navLinks.map((link, index) => (
-            <a
-              key={index}
-              className="nav-link text-blue-500 hover:bg-blue-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-              href={link.link}
-            >
-              {link.title}
-            </a>
-          ))}
-          <button
-            onClick={handleSignOut}
-            type="submit"
-            className="bg-blue-500 w-32 "
-          >
-            LOGOUT
-          </button>
+    <div>
+      {user ? (
+        <div className="fixed w-screen h-20 flex items-center justify-between border-b-2 border-solid border-blue-500 bg-white">
+          <div className="w-48 h-fit ml-10">
+            <img src="/logo.png" alt="Logo" className="" />
+          </div>
+          <div className="flex items-center justify-center mr-10">
+            <div className="ml-10 flex items justify-between">
+              {navLinks.map((link, index) => (
+                <a
+                  key={index}
+                  className="nav-link text-blue-500 hover:bg-blue-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  href={link.link}
+                >
+                  {link.title}
+                </a>
+              ))}
+              <button
+                onClick={handleSignOut}
+                type="submit"
+                className="bg-blue-500 w-32 "
+              >
+                LOGOUT
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <h1>Please Log In to access the admin portal</h1>
+        </div>
+      )}
     </div>
   );
 };
